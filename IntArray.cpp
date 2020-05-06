d65a3d80f9e48db261eda9322afae04209c34270
Vladislav Bogdanov <36765224+vlhell@users.noreply.github.com>
2020-05-06 14:57:01 +0300

    added cpp

---
+++ IntArray.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+class IntArray {
+public:
+    explicit IntArray(size_t size)
+        : size_(size)
+        , data(new int[size])
+    { }
+
+    ~IntArray() {
+        delete [] data;
+    }
+
+    int & get(size_t i) {
+        return data_[i];
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+    void set(size_t i, int num) {
+        data_[i] = num;
+    }
+
+    void resize(int nsize) {
+        int * ndata = new int[nsize];
+        size_t n = nsize > size_ ? size_ : nsize;
+
+        for (int i = 0; i != n; ++i)
+            ndata[i] = data_[i];
+
+        delete [] data_;
+        data_ = ndata;
+        size_ = nsize;
+    }
+
+    IntArray(IntArray const& a) : //copying
+        size_(a.size), data(new int[size_])
+    {
+            for (size_t i = 0; i != size_; ++i) {
+                data_[i] = a.data_[i];
+
+            }
+    }
+
+    IntArray & operator=(IntArray const& a) { //assignment
+        if (this != &a) {
+            delete [] data_;
+            size_ = a.size_;
+            data_ = new int[size_];
+            for (size_t i = 0; i != size_; ++i)
+                data_[i] = a.data_[i];
+        }
+        return *this;
+    }
+private:
+    size_t size_;
+    int * data_;
+};
+
+int main() {
+    IntArray * pa = new IntArray(10);
+    delete pa;
+
+    IntArray * pb = 
+        (IntArray *)malloc(sizeof(IntArray));
+
+    IntArray * pc = new IntArray[10];
+    delete [] pc;
+
+    IntArray a1(10);
+    IntArray a2(20);
+    IntArray a3 = a1; // copying
+    a2 = a1; // assignment
+    return 0;
+}
