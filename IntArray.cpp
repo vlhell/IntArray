077b325e41b09f12711275de5cfd04bfb5c9d86a
Vladislav Bogdanov <36765224+vlhell@users.noreply.github.com>
2020-05-09 15:29:05 +0300

    upd cpp

--- IntArray.cpp
+++ IntArray.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,7 +8,23 @@ public:
     explicit IntArray(size_t size)
         : size_(size)
         , data(new int[size])
-    { }
+    { 
+        for (int i = 0; i != size_; ++i)
+            data[i] = 0;
+    }
+
+    IntArray(IntArray const& a) : //copying
+        size_(a.size), data(new int[size_])
+    {
+            for (size_t i = 0; i != size_; ++i)
+                data_[i] = a.data_[i];
+    }
+
+    IntArray & operator=(IntArray const& a) { //assignment
+        if (this != &a)
+            IntArray(a).swap2(*this);
+        return *this;
+    }
 
     ~IntArray() {
         delete [] data;
@@ -17,9 +34,7 @@ public:
         return data_[i];
     }
 
-    size_t size() const {
-        return size_;
-    }
+    size_t size() const { return size_; }
 
     void set(size_t i, int num) {
         data_[i] = num;
@@ -37,26 +52,34 @@ public:
         size_ = nsize;
     }
 
-    IntArray(IntArray const& a) : //copying
-        size_(a.size), data(new int[size_])
-    {
-            for (size_t i = 0; i != size_; ++i) {
-                data_[i] = a.data_[i];
+    void resize2(int nsize) {
+        IntArray t(nsize);
+        size_t n = nsize > size_ ? size_ : nsize;
 
-            }
+        for (int i = 0; i != n; ++i)
+            t.data_[i] = data_[i];
+
+        swap(t);
+    }
+    
+    void swap(IntArray & a) {
+        size_t const t1 = size_;
+        size_ = a.size_;
+        a.size_ = t1;
+
+        int * const t2 = data_;
+        data_ = a.data_;
+        a.data_ = t2;
     }
 
-    IntArray & operator=(IntArray const& a) { //assignment
-        if (this != &a) {
-            delete [] data_;
-            size_ = a.size_;
-            data_ = new int[size_];
-            for (size_t i = 0; i != size_; ++i)
-                data_[i] = a.data_[i];
-        }
-        return *this;
+    void swap2(IntArray & a) {
+        std::swap(size_, a.size_);
+        std::swap(data_, a.data_);
     }
 private:
+    // IntArray(IntArray const& a); // prohibition of copying objects
+    // IntArray & operator=(IntArray const& a); // prohibition of copying objects
+
     size_t size_;
     int * data_;
 };
